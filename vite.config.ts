7205f9798f605fc3904a4829b2fab5fc0c7eb016
madashivakarthikgoud <madashivakarthikgoud@gmail.com>
2025-05-12 13:15:59 +0530

    made changes according to render

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ArrowOsExtended/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
